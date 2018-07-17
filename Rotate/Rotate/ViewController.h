2f3c92940d2e065471b93c0ad362405839a7ad2b
lly <bjlilingyu@corp.netease.com>
2018-07-17 20:01:09 +0800

    rotate

---
+++ Rotate/Rotate/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Rotate
+//
+//  Created by lly on 2018/7/17.
+//  Copyright © 2018年 lly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
---
+++ Rotate/Rotate/ViewController.m
@@ -0,0 +1,191 @@
+//
+//  ViewController.m
+//  Rotate
+//
+//  Created by lly on 2018/7/17.
+//  Copyright © 2018年 lly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<GLKViewDelegate>{
+    dispatch_source_t _timer;
+}
+
+@property (nonatomic, strong) EAGLContext *mContext;
+@property (nonatomic, strong) GLKBaseEffect *mEffect;
+
+@property (nonatomic , assign) int mCount;
+@property (nonatomic , assign) float mDegreeX;
+@property (nonatomic , assign) float mDegreeY;
+@property (nonatomic , assign) float mDegreeZ;
+
+
+@property (nonatomic , assign) BOOL mBoolX;
+@property (nonatomic , assign) BOOL mBoolY;
+@property (nonatomic , assign) BOOL mBoolZ;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupContext];
+    [self render];
+}
+
+
+- (void)setupContext{
+    
+    self.mContext = [[EAGLContext alloc]initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    GLKView *view = (GLKView *)self.view;
+    view.delegate = self;
+    view.context = self.mContext;
+    view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    
+    [EAGLContext setCurrentContext:self.mContext];
+}
+
+- (void)render{
+    
+    //启用了之后，OpenGL在绘制的时候就会检查，当前像素前面是否有别的像素，如果别的像素挡道了它，那它就不会绘制，也就是说，OpenGL就只绘制最前面的一层。
+    glEnable(GL_DEPTH_TEST);
+    
+    //顶点数据，前三个是顶点坐标， 中间三个是顶点颜色，    最后两个是纹理坐标
+    GLfloat attrArr[] =
+    {
+        -0.5f, 0.5f, 0.0f,      0.0f, 0.0f, 0.5f,       0.0f, 1.0f,//左上
+        0.5f, 0.5f, 0.0f,       0.0f, 0.5f, 0.0f,       1.0f, 1.0f,//右上
+        -0.5f, -0.5f, 0.0f,     0.5f, 0.0f, 1.0f,       0.0f, 0.0f,//左下
+        0.5f, -0.5f, 0.0f,      0.0f, 0.0f, 0.5f,       1.0f, 0.0f,//右下
+        0.0f, 0.0f, 1.0f,       1.0f, 1.0f, 1.0f,       0.5f, 0.5f,//顶点
+    };
+    
+    //顶点索引
+    GLuint indices[] =
+    {
+        0, 3, 2,
+        0, 1, 3,
+        0, 2, 4,
+        0, 4, 1,
+        2, 3, 4,
+        1, 4, 3,
+    };
+    
+    self.mCount = sizeof(indices) / sizeof(GLuint);
+    
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(attrArr), attrArr, GL_STATIC_DRAW);//顶点数据
+    
+    GLuint index;
+    glGenBuffers(1, &index);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//顶点索引
+    
+    //顶点坐标
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 8, (GLfloat *)NULL);
+    //顶点颜色
+    glEnableVertexAttribArray(GLKVertexAttribColor);
+    glVertexAttribPointer(GLKVertexAttribColor, 3, GL_FLOAT, GL_FALSE, 4 * 8, (GLfloat *)NULL + 3);
+    //顶点对应的纹理坐标
+    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 4 * 8, (GLfloat *)NULL + 6);
+    
+    //设置效果器
+    [self setupEffect];
+    //设置初始位置
+    [self setupMatrix];
+    //设置timer
+    [self setupTimer];
+    
+}
+
+- (void)setupEffect{
+    
+    //纹理
+    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"wk" ofType:@"JPG"];
+    NSDictionary* options = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderOriginBottomLeft, nil];
+    GLKTextureInfo* textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
+    
+    //着色器
+    self.mEffect = [[GLKBaseEffect alloc] init];
+    self.mEffect.texture2d0.enabled = GL_TRUE;
+    self.mEffect.texture2d0.name = textureInfo.name;
+    
+}
+
+- (void)setupMatrix{
+    
+    //初始的投影
+    CGSize size = self.view.bounds.size;
+    float aspect = fabs(size.width / size.height);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(90.0), aspect, 0.1f, 10.f);
+    projectionMatrix = GLKMatrix4Scale(projectionMatrix, 1.0f, 1.0f, 1.0f);
+    self.mEffect.transform.projectionMatrix = projectionMatrix;
+    
+    GLKMatrix4 modelViewMatrix = GLKMatrix4Translate(GLKMatrix4Identity, 0.0f, 0.0f, -2.0f);
+    self.mEffect.transform.modelviewMatrix = modelViewMatrix;
+    
+}
+
+- (void)setupTimer{
+    //定时器
+    double delayInSeconds = 0.1;
+    _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(_timer, DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC, 0.0);
+    dispatch_source_set_event_handler(_timer, ^{
+        self.mDegreeX += 0.1 * self.mBoolX;
+        self.mDegreeY += 0.1 * self.mBoolY;
+        self.mDegreeZ += 0.1 * self.mBoolZ;
+        
+    });
+    dispatch_resume(_timer);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)rotateX:(id)sender {
+    self.mBoolX = !self.mBoolX;
+}
+- (IBAction)rotateY:(id)sender {
+    self.mBoolY = !self.mBoolY;
+}
+- (IBAction)rotateZ:(id)sender {
+    self.mBoolZ = !self.mBoolZ;
+}
+
+- (void)update{
+    
+    GLKMatrix4 modelViewMatrix = GLKMatrix4Translate(GLKMatrix4Identity, 0.0f, 0.0f, -2.0f);
+    
+    modelViewMatrix = GLKMatrix4RotateX(modelViewMatrix, self.mDegreeX);
+    modelViewMatrix = GLKMatrix4RotateY(modelViewMatrix, self.mDegreeY);
+    modelViewMatrix = GLKMatrix4RotateZ(modelViewMatrix, self.mDegreeZ);
+    
+    self.mEffect.transform.modelviewMatrix = modelViewMatrix;
+}
+
+#pragma mark - GLKView Delegate
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect{
+    
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    [self.mEffect prepareToDraw];
+    glDrawElements(GL_TRIANGLES, self.mCount, GL_UNSIGNED_INT, 0);
+    
+}
+
+
+@end
