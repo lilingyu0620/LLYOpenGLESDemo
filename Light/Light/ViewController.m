a7c49e62dacd9452196ca561ec154d488502b5d0
lly <bjlilingyu@corp.netease.com>
2018-07-24 20:19:19 +0800

    light

--- Light/Light/ViewController.m
+++ Light/Light/ViewController.m
@@ -7,23 +7,80 @@
 //
 
 #import "ViewController.h"
+#import "AGLKVertexAttribArrayBuffer.h"
+#import "sceneUtil.h"
 
-@interface ViewController ()
+@interface ViewController ()<GLKViewDelegate>
+
+@property (nonatomic, strong) EAGLContext *mContext;
+
+@property (nonatomic, strong) GLKBaseEffect *baseEffect;
+@property (nonatomic, strong) GLKBaseEffect *extraEffect;
+
+@property (nonatomic, strong) AGLKVertexAttribArrayBuffer *vertexBuffer;
+@property (nonatomic, strong) AGLKVertexAttribArrayBuffer *extraBuffer;
+
+@property (nonatomic, assign, getter=isUseFaceNormals) BOOL useFaceNormals;
+@property (nonatomic, assign, getter=isDrawFaceNormals) BOOL drawFaceNormals;
+@property (nonatomic, assign) CGFloat zPosition;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    
+    SceneTriangle triangles[NUM_FACES];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
+- (void)setupContext{
+    
+    //新建OpenGLES 上下文
+    self.mContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    GLKView* view = (GLKView *)self.view;
+    view.context = self.mContext;
+    view.delegate = self;
+    view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    
+    [EAGLContext setCurrentContext:self.mContext];
+    
+}
+
+- (void)setupEffect{
+    
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)useFaceNormal:(id)sender {
+    
+}
+
+- (IBAction)drawFaceNormal:(id)sender {
+    
+}
+- (IBAction)zPositionValueChanged:(id)sender {
+    
+}
+
+#pragma mark - GLKViewDelegate
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect{
+    
+    
+}
 
 @end
