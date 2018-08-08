ecedce3e946dc2aebe4171295f22abc42551de70
lly <bjlilingyu@corp.netease.com>
2018-08-08 19:32:15 +0800

    loadmodelplist

---
+++ LoadModelPlist/LoadModelPlist/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  LoadModelPlist
+//
+//  Created by lly on 2018/8/8.
+//  Copyright © 2018年 lly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
